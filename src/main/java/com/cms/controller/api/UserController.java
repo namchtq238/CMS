9ef5e105071b4354fae95e75c88d5ff8197482fc
matsu <vikto132@gmail.com>
2022-04-09 03:59:26 +0700

    login

--- src/main/java/com/cms/controller/api/UserController.java
+++ src/main/java/com/cms/controller/api/UserController.java
@@ -5,7 +5,13 @@ import com.cms.controller.request.UserReq;
 import com.cms.controller.response.UserInfoRes;
 import com.cms.controller.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +21,16 @@ public class UserController {
 
     @Autowired
     UserService userService;
+    @Autowired
+    AuthenticationManager authenticate;
+    @PostMapping("/login")
+    public ResponseEntity<String> authenticateUser(@RequestBody UserReq userReq){
+        Authentication authentication = authenticate.authenticate(new UsernamePasswordAuthenticationToken(
+                userReq.getUsername(), userReq.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<>("User signed-in successfully!.", HttpStatus.OK);
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> createNewAccount(@Validated @RequestBody UserReq user){
