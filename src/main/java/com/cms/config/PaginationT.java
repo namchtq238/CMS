62a4a2fe804ff11fd127a5e0242b0f573918b807
matsu <vikto132@gmail.com>
2022-04-09 02:57:23 +0700

    add pagination và corsconfig

--- src/main/java/com/cms/config/PaginationT.java
+++ src/main/java/com/cms/config/PaginationT.java
@@ -1,4 +1,30 @@
 package com.cms.config;
 
-public class PaginationT {
+import java.util.Collection;
+import java.util.Collections;
+
+class PaginationT<E> {
+    long total = 0L;
+    Collection<E> elements;
+
+    public PaginationT() {
+    }
+
+    public PaginationT<E> setItems(Collection<E> elements) {
+        this.elements = (Collection)(elements == null ? Collections.emptyList() : elements);
+        return this;
+    }
+
+    public Collection<E> getItems() {
+        return this.elements;
+    }
+
+    public PaginationT<E> setTotal(long total) {
+        this.total = total;
+        return this;
+    }
+
+    public long getTotal() {
+        return this.total == 0L ? (long)this.elements.size() : this.total;
+    }
 }
