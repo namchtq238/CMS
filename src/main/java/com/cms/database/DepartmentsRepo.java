bf33700a72070f0133887f37da906c198214f718
thinh12 <bmtnt09042000@gmail.com>
2022-04-09 21:49:58 +0700

    config google storage

--- src/main/java/com/cms/database/DepartmentsRepo.java
+++ src/main/java/com/cms/database/DepartmentsRepo.java
@@ -1,9 +1,17 @@
 package com.cms.database;
 
+import com.cms.database.converter.DepartmentConverter;
 import com.cms.entity.Departments;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentsRepo extends JpaRepository<Departments, Long> {
+    @Query(value = "select id as from departments " +
+            "left join", nativeQuery = true)
+    Page<DepartmentConverter> getListDepartments(String keyWord, Pageable pageable);
+
 }
