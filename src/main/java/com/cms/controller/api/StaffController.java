35b3ba751fb43e69d0791bd819ccd24fa99ce68a
namchtq238 <kaizuto123@gmail.com>
2022-04-12 01:05:41 +0700

    CRUD Staff

---
+++ src/main/java/com/cms/controller/api/StaffController.java
@@ -0,0 +1,65 @@
+package com.cms.controller.api;
+
+import com.cms.config.dto.ResponseHelper;
+import com.cms.controller.request.StaffReq;
+import com.cms.controller.service.StaffService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/staff")
+public class StaffController {
+    @Autowired
+    ResponseHelper responseHelper;
+    @Autowired
+    StaffService staffService;
+
+    @GetMapping()
+    public ResponseEntity<?> getallStaff() {
+        try {
+            return responseHelper.successResp(staffService.getAllStaff(), HttpStatus.OK);
+        } catch (Exception e) {
+            return responseHelper.infoResp(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping()
+    public ResponseEntity<?> addNewStaff(@RequestBody StaffReq staffReq) {
+        try {
+            return responseHelper.successResp(staffService.createNewStaff(staffReq), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return responseHelper.infoResp(e.getMessage(), HttpStatus.NO_CONTENT);
+        }
+
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getStaff(@PathVariable Long id){
+        try {
+            return responseHelper.successResp(staffService.getStaff(id), HttpStatus.OK);
+        } catch (Exception e) {
+            return responseHelper.infoResp(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateStaff(@PathVariable Long id, @RequestBody StaffReq staffReq){
+        try {
+            return responseHelper.successResp(staffService.update(id, staffReq), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return responseHelper.infoResp(e.getMessage(), HttpStatus.NO_CONTENT);
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteStaff(@PathVariable Long id){
+        try {
+            return responseHelper.successResp(staffService.delete(id), HttpStatus.OK);
+        }
+        catch (Exception e){
+            return responseHelper.infoResp(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+}
--- src/main/java/com/cms/service/StaffServiceImp.java
+++ src/main/java/com/cms/service/StaffServiceImp.java
@@ -1,12 +1,21 @@
 package com.cms.service;
 
+import com.cms.controller.request.StaffReq;
+import com.cms.controller.response.StaffRes;
 import com.cms.controller.service.StaffService;
 import com.cms.controller.service.UserService;
 import com.cms.database.StaffRepo;
+import com.cms.database.UserRepository;
 import com.cms.entity.Staff;
 import com.cms.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class StaffServiceImp implements StaffService {
@@ -14,10 +23,88 @@ public class StaffServiceImp implements StaffService {
     UserService userService;
     @Autowired
     StaffRepo staffRepo;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public Staff getCurrentStaff() {
         User user = userService.getCurrentUser();
         return staffRepo.getStaffByUser(user);
+    }
+
+    @Override
+    public List<StaffRes> getAllStaff() {
+        List<Staff> staffList = staffRepo.getAll();
+        return staffList.stream().map(staff -> {
+            StaffRes res = new StaffRes();
+            res.setStaffId(staff.getId());
+            res.setPosition(staff.getPosition());
+            res.setUserId(staff.getUser().getId());
+            return res;
+        }).collect(Collectors.toList());
+
+    }
+
+    @Override
+    @Transactional(rollbackOn = RuntimeException.class)
+    public StaffReq createNewStaff(StaffReq staffReq) {
+        User user = new User();
+        Staff staff = new Staff();
+        user.setPassword(passwordEncoder.encode(staffReq.getPassword()));
+        user.setUserName(staffReq.getUsername());
+        user.setAddress(staffReq.getAddress());
+        user.setEmail(staffReq.getEmail());
+        user.setRole(1);
+        user.setName(staffReq.getName());
+        userRepository.save(user);
+        staff.setPosition(staffReq.getPosition());
+        staff.setUser(user);
+        staffRepo.save(staff);
+        return staffReq;
+    }
+
+    @Override
+    public StaffRes getStaff(Long id) {
+        Optional<Staff> opt = staffRepo.findById(id);
+        if (opt.isEmpty())            throw new RuntimeException("Not Found");
+        Staff staff = opt.get();
+        StaffRes res = new StaffRes();
+        res.setStaffId(staff.getId());
+        res.setPosition(staff.getPosition());
+        res.setUserId(staff.getUser().getId());
+        return res;
+    }
+
+    @Override
+    @Transactional(rollbackOn = RuntimeException.class)
+    public boolean delete(Long id) {
+        Optional<Staff> opt = staffRepo.findById(id);
+        if (opt.isEmpty()) throw new RuntimeException("Not Found");
+        userRepository.delete(opt.get().getUser());
+        staffRepo.delete(opt.get());
+        return true;
+    }
+
+    @Override
+    @Transactional(rollbackOn = RuntimeException.class)
+    public StaffReq update(Long id, StaffReq staffReq) {
+        Optional<Staff> opt = staffRepo.findById(id);
+        if (opt.isEmpty()) throw new RuntimeException("Not Found");
+        Staff staff = opt.get();
+        staff.setPosition(staff.getPosition());
+        Optional<User> optionalUser = userRepository.findById(staff.getUser().getId());
+        if (optionalUser.isEmpty()) throw new RuntimeException("Not Found");
+        User user = optionalUser.get();
+        user.setName(staffReq.getName());
+        user.setEmail(staffReq.getEmail());
+        user.setRole(staffReq.getRole());
+        user.setUserName(staffReq.getUsername());
+        user.setPassword(passwordEncoder.encode(staffReq.getPassword()));
+        user.setAddress(staffReq.getAddress());
+        userRepository.save(user);
+        staffRepo.save(staff);
+        return staffReq;
     }
 }
