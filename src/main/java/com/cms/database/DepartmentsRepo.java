946945d86f2bb6e3278afa8a081a463fb319a30d
thinh12 <thinhtn.it@tripi.vn>
2022-04-16 01:30:22 +0700

    fix bug

--- src/main/java/com/cms/database/DepartmentsRepo.java
+++ src/main/java/com/cms/database/DepartmentsRepo.java
@@ -10,19 +10,19 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentsRepo extends JpaRepository<Departments, Long> {
-    @Query(value = "select departments.id as id, " +
-            "departments.name as name, " +
-            "departments.start_date as startDate," +
-            "departments.closure_date_idea as closureDateIdea," +
-            "departments.closure_date as closureDate, " +
-            "users.name as qaName  " +
-            "from departments d " +
-            "left join qa on departments.id = qa.department_id " +
-            "left join users on qa.user_id = users.id " +
+    @Query(value = "select `departments`.id as id, " +
+            "`departments`.name as name, " +
+            "`departments`.start_date as startDate," +
+            "`departments`.closure_date_idea as closureDateIdea," +
+            "`departments`.closure_date as closureDate, " +
+            "`users`.name as qaName  " +
+            "from `departments` " +
+            "left join `qa` on `departments`.id = `qa`.department_id " +
+            "left join `users` on `qa`.user_id = `users`.id " +
             "where 1 = 1 " +
-            "AND(?1 is null or departments.id like %?1% " +
-            "OR departments.name like %?1% " +
-            "OR users.name like %?1%)", nativeQuery = true)
+            "AND(?1 is null or `departments`.id like %?1% " +
+            "OR `departments`.name like %?1% " +
+            "OR `users`.name like %?1%)", nativeQuery = true)
     Page<DepartmentConverter> getListDepartments(String keyWord, Pageable pageable);
 
 }
