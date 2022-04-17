74ef3894df4de7bf50b8cbcc9c2c5e9f69d762a5
thinh12 <thinhtn.it@tripi.vn>
2022-04-17 11:34:45 +0700

    fix departments

--- src/main/java/com/cms/database/DepartmentsRepo.java
+++ src/main/java/com/cms/database/DepartmentsRepo.java
@@ -17,12 +17,11 @@ public interface DepartmentsRepo extends JpaRepository<Departments, Long> {
             "`departments`.closure_date as closureDate, " +
             "`users`.name as qaName  " +
             "from `departments` " +
-            "left join `qa` on `departments`.id = `qa`.department_id " +
-            "left join `users` on `qa`.user_id = `users`.id " +
+            "left join `users` on `departments`.user_id = `users`.id " +
             "where 1 = 1 " +
             "AND(?1 is null or `departments`.id like %?1% " +
             "OR `departments`.name like %?1% " +
-            "OR `users`.name like %?1%)", nativeQuery = true)
+            "OR `users`.name like %?1%) " +
+            "AND `users`.role = 3", nativeQuery = true)
     Page<DepartmentConverter> getListDepartments(String keyWord, Pageable pageable);
-
 }
